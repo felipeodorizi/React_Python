5cde2f212854ee6607dbea56452629cf3667d5bc
framework-CPTEC <rameworkcptec@gmail.com>
2025-04-23 16:00:17 -0300

    Retirando erro loop Imagem

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MakeImage from "./components/MakeImage";
 
 
 function App() {
-  //const [imagemUrl, setImagemUrl] = useState("");
+  const [imagemUrl, setImagemUrl] = useState("");
 
   const [coordinates, setCoordinates] = useState(
     { LatitudeMin: -34.44, LatitudeMax: -21.43, LongitudeMin: 301.14, LongitudeMax: 320.57 }
@@ -13,27 +13,45 @@ function App() {
 
    function onAddCoordinatesSubmit(LatitudeMin, LatitudeMax, LongitudeMin, LongitudeMax) {
      setCoordinates({ LatitudeMin, LatitudeMax, LongitudeMin, LongitudeMax });
+
+    const flaskUrl = `http://127.0.0.1:5000/imagem?param1=${LatitudeMin}&param2=${LatitudeMax}&param3=${LongitudeMin}&param4=${LongitudeMax}`;
+
+        console.log("E aí, isso aqui roda sempre que o componente renderizar!");
+           // Obtém a imagem do servidor Flask
+    fetch(flaskUrl)
+      .then((response) => {
+        if (response.ok) {
+            return response.blob(); // Converte a resposta em um Blob
+        }
+            throw new Error("Erro ao buscar imagem");
+      })
+      .then((blob) => {
+        const url = URL.createObjectURL(blob); // Cria uma URL temporária para exibir a imagem
+        setImagemUrl(url); // Define a URL como estado para renderizar no React
+      })
+      .catch((error) => console.error(error));
+
    }
 
 
-  // useEffect(() => {
-  //   // URL do servidor Flask
-  //   const flaskUrl = "http://127.0.0.1:5000/imagem?param1=-34.44&param2=-21.43&param3=301.14&param4=320.57";
+  useEffect(() => {
+    // URL do servidor Flask
+    const flaskUrl = "http://127.0.0.1:5000/imagem?param1=-34.44&param2=-21.43&param3=301.14&param4=320.57";
 
-  //   // Obtém a imagem do servidor Flask
-  //   fetch(flaskUrl)
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.blob(); // Converte a resposta em um Blob
-  //       }
-  //       throw new Error("Erro ao buscar imagem");
-  //     })
-  //     .then((blob) => {
-  //       const url = URL.createObjectURL(blob); // Cria uma URL temporária para exibir a imagem
-  //       setImagemUrl(url); // Define a URL como estado para renderizar no React
-  //     })
-  //     .catch((error) => console.error(error));
-  // }, []);
+    // Obtém a imagem do servidor Flask
+    fetch(flaskUrl)
+      .then((response) => {
+        if (response.ok) {
+          return response.blob(); // Converte a resposta em um Blob
+        }
+        throw new Error("Erro ao buscar imagem");
+      })
+      .then((blob) => {
+        const url = URL.createObjectURL(blob); // Cria uma URL temporária para exibir a imagem
+        setImagemUrl(url); // Define a URL como estado para renderizar no React
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
 
 
@@ -52,9 +70,12 @@ function App() {
         <ShowCoordinates coordinates={coordinates} />
       </div>
 
-      <MakeImage coordinates={coordinates}/>
 
-  
+    <div>
+      <h1>Imagem do Flask</h1>
+      {imagemUrl ? <img src={imagemUrl} alt="Gerado pelo Flask" /> : <p>Carregando imagem...</p>}
+    </div>
+
 
     </div>
   );
